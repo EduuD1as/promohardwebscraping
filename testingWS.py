c904f4a51e2a9109bee40c680a9854526a347299
Eduardo Dias Bianchini <edudiasbianchini@gmail.com>
2024-10-26 18:18:06 -0300

    ajustes de elementos

--- testingWS.py
+++ testingWS.py
@@ -7,53 +7,53 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 
-# configuração do driver do Chrome
+# Configuração do driver do Chrome
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-url = 'https://www.terabyteshop.com.br/hardware/placas-de-video'
+url = 'https://www.kabum.com.br/hardware/placa-de-video-vga'
 driver.get(url)
 
-# função para extrair os dados de cada placa (por página)
+# Função para extrair os dados de cada placa (por página)
 def extrair_placas(driver):
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.commerce_columns_item_inner')))
-    placas = driver.find_elements(By.CSS_SELECTOR, 'div.commerce_columns_item_inner')
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.sc-27518a44-5')))
+    placas = driver.find_elements(By.CSS_SELECTOR, 'div.sc-27518a44-5')
     for placa in placas:
         try:
-            marca = placa.find_element(By.CSS_SELECTOR, 'h2')
+            marca = placa.find_element(By.CSS_SELECTOR, 'span.sc-d79c9c3f-0')
             print(marca.text)
         except Exception as e:
             print(f"Erro ao extrair marca: {e}")
             continue
 
-# extrai as placas da primeira página
+# Extrai as placas da primeira página
 extrair_placas(driver)
 
-# try para navegar nas próximas páginas e extrair as placas de vídeo
+# Tente navegar para as próximas páginas e extrair as placas de vídeo
 while True:
     try:
         # Verifique se o botão "Próxima Página" está presente
         botao_proxima_pagina = WebDriverWait(driver, 15).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.arrow-down btn btn-pdmore'))
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.nextLink'))
         )
         print("Botão 'Próxima Página' encontrado.")
         
-        # movendo até o botão para garantir que esteja visível
+        # Movendo até o botão para garantir que esteja visível
         actions = ActionChains(driver)
         actions.move_to_element(botao_proxima_pagina).perform()
         
-        # clicando no botão "Próxima Página"
+        # Clique no botão "Próxima Página"
         botao_proxima_pagina.click()
         print("Botão 'Próxima Página' clicado.")
         
-        # aguardar o carregamento da nova página
+        # Aguarde até que a nova página seja carregada
+        WebDriverWait(driver, 15).until(EC.staleness_of(botao_proxima_pagina))
         WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.commerce_columns_item_inner'))
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.sc-27518a44-5'))
         )
         print("Nova página carregada.")
         
-        # extrai novamente as placas de vídeo
+        # Extrai novamente as placas de vídeo
         extrair_placas(driver)
-
     except Exception as e:
         if "element could not be scrolled into view" in str(e):
             print("O botão 'Próxima Página' não pôde ser encontrado. Finalizando a navegação.")
