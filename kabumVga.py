a7decc1e43c8d4c7cd65070b9c8c0ea431bcbc44
Eduardo Dias Bianchini <edudiasbianchini@gmail.com>
2024-10-29 00:17:38 -0300

    .

--- kabumVga.py
+++ kabumVga.py
@@ -15,14 +15,19 @@ driver.maximize_window()
 
 # Função para extrair os dados de cada placa (por página)
 def extrair_placas(driver):
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.sc-27518a44-5')))
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.sc-27518a44-5')))
     placas = driver.find_elements(By.CSS_SELECTOR, 'div.sc-27518a44-5')
     for placa in placas:
         try:
-            nomeMarca = placa.find_element(By.CSS_SELECTOR, 'span.sc-d79c9c3f-0')
-            print(nomeMarca.text)
+            # Extraindo o nome da placa
+            nomePlaca = placa.find_element(By.CSS_SELECTOR, 'span.sc-d79c9c3f-0')
+            print(f"Nome: {nomePlaca.text}")
+            
+            # Extraindo o preço da placa
+            precoPlaca = placa.find_element(By.CSS_SELECTOR, 'span.sc-3b515ca1-2')
+            print(f"Preço: {precoPlaca.text}")
         except Exception as e:
-            print(f"Erro ao extrair marca: {e}")
+            print(f"Erro ao extrair dados: {e}")
             continue
 
 # Extrai as placas da primeira página
@@ -32,7 +37,7 @@ extrair_placas(driver)
 while True:
     try:
         # Verifique se o botão "Próxima Página" está presente (clicável)
-        botao_proxima_pagina = WebDriverWait(driver, 20).until(
+        botao_proxima_pagina = WebDriverWait(driver, 15).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.nextLink'))
         )
         print("Botão 'Próxima Página' encontrado.")
@@ -42,8 +47,8 @@ while True:
         print("Botão 'Próxima Página' clicado.")
         
         # Aguardar o carregamento da nova página
-        WebDriverWait(driver, 20).until(EC.staleness_of(botao_proxima_pagina))
-        WebDriverWait(driver, 20).until(
+        WebDriverWait(driver, 15).until(EC.staleness_of(botao_proxima_pagina))
+        WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'div.sc-27518a44-5'))
         )
         print("Nova página carregada.")
